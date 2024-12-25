3efabd3afdfed3ae0eaf608f7dcee13a4847c391
Bermet Beisheeva <bermet160606@gmail.com>
2024-12-25 19:28:26 +0600

    Feature/Payment for

---
+++ src/main/java/payment/PaymentForm.java
@@ -0,0 +1,8 @@
+package payment;
+
+public class PaymentForm {
+    public static void main(String[] args) {
+        System.out.println("Payment form ");
+        System.out.println("hey yoo");
+    }
+}
